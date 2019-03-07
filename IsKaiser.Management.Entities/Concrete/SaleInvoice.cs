f25d46a7a344db390f41ddbe0be6bb8787dff439
halil ibrahim duymaz <halildymz16@hotmail.com>
2019-03-07 13:52:37 +0300

    Added SaleInvoice Entity

---
+++ IsKaiser.Management.Entities/Concrete/SaleInvoice.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsKaiser.Management.Entities.Concrete
+{
+    public class SaleInvoice
+    {
+        public int SaleId { get; set; }
+        public int InvoiceId { get; set; }
+        public string RefNumber { get; set; }
+        public short ExpiryDate { get; set; }
+        public string RequestedBy { get; set; }
+        public string OrderedBy { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string OrderNo { get; set; }
+    }
+}
