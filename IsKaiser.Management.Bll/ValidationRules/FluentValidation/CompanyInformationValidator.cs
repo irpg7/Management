2035f2bb668fb489131a9ff4c94edd61b26b0857
halil ibrahim duymaz <halildymz16@hotmail.com>
2019-03-21 17:47:15 +0300

    Added Validators for New entities

---
+++ IsKaiser.Management.Bll/ValidationRules/FluentValidation/CompanyInformationValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using IsKaiser.Management.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsKaiser.Management.Bll.ValidationRules.FluentValidation
+{
+    public class CompanyInformationValidator:AbstractValidator<CompanyInformation>
+    {
+        public CompanyInformationValidator()
+        {
+
+        }
+    }
+}
