68f00f6d4022f5a2696b9d76da8f1bcd1e18b966
halil ibrahim duymaz <halildymz16@hotmail.com>
2019-03-21 17:17:38 +0300

    Added Ef operations for CompanyBankAccount,CompanyInfo

---
+++ IsKaiser.Management.Dal/Concrete/EntityFramework/EfCompanyBankAccountDal.cs
@@ -0,0 +1,12 @@
+﻿using IsKaiser.Management.Core.DataAccess.EntityFramework;
+using IsKaiser.Management.Dal.Abstract;
+using IsKaiser.Management.Entities.Concrete;
+using System;
+
+namespace IsKaiser.Management.Dal.Concrete.EntityFramework
+{
+    public class EfCompanyBankAccountDal : EfRepositoryBase<CompanyBankAccount, IsKaiserContext>, ICompanyBankAccountDal
+    {
+
+    }
+}
---
+++ IsKaiser.Management.Dal/Concrete/EntityFramework/EfCompanyInformationDal.cs
@@ -0,0 +1,13 @@
+﻿using IsKaiser.Management.Core.DataAccess.EntityFramework;
+using IsKaiser.Management.Dal.Abstract;
+using IsKaiser.Management.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsKaiser.Management.Dal.Concrete.EntityFramework
+{
+    public class EfCompanyInformationDal:EfRepositoryBase<CompanyInformation,IsKaiserContext>,ICompanyInformationDal
+    {
+    }
+}
