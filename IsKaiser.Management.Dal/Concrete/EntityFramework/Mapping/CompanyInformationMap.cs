31f41ec259d2e0299b9bd672e70ac2af03cfe43c
halil ibrahim duymaz <halildymz16@hotmail.com>
2019-03-21 17:20:29 +0300

    Added Mapping For CompanyBankAccount,CompanyInfo

---
+++ IsKaiser.Management.Dal/Concrete/EntityFramework/Mapping/CompanyInformationMap.cs
@@ -0,0 +1,17 @@
+﻿using IsKaiser.Management.Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsKaiser.Management.Dal.Concrete.EntityFramework.Mapping
+{
+    public class CompanyInformationMap : IEntityTypeConfiguration<CompanyInformation>
+    {
+        public void Configure(EntityTypeBuilder<CompanyInformation> builder)
+        {
+            builder.HasKey(ci => ci.Id);
+        }
+    }
+}
