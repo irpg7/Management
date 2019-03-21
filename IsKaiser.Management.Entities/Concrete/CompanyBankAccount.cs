720fd6dfcc17a78c701375be3bdfe8470e15a41a
halil ibrahim duymaz <halildymz16@hotmail.com>
2019-03-21 17:10:10 +0300

    Added CompanyInfo,CompanyBankAccount Entities

---
+++ IsKaiser.Management.Entities/Concrete/CompanyBankAccount.cs
@@ -0,0 +1,18 @@
+﻿using IsKaiser.Management.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsKaiser.Management.Entities.Concrete
+{
+    public class CompanyBankAccount:IEntity
+    {
+        public int AccountId { get; set; }
+        public string BankName { get; set; }
+        public string BranchOffice { get; set; }
+        public string LiraIban { get; set; }
+        public string EuroIban { get; set; }
+        public string DollarIban { get; set; }
+        public string SwiftCode { get; set; }
+    }
+}
---
+++ IsKaiser.Management.Entities/Concrete/CompanyInformation.cs
@@ -0,0 +1,18 @@
+﻿using IsKaiser.Management.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsKaiser.Management.Entities.Concrete
+{
+    public class CompanyInformation:IEntity
+    {
+        public int Id { get; set; }
+        public int BankAccountId { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string TaxDepartment { get; set; }
+        public string TaxNo { get; set; }
+        
+    }
+}
