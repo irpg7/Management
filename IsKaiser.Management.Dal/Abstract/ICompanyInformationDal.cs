5b5ee8d7ad14890a3df7f29752251ad93a3ac602
halil ibrahim duymaz <halildymz16@hotmail.com>
2019-03-21 17:14:21 +0300

    Added CompanyInfo,BankAccount Dal Interfaces

---
+++ IsKaiser.Management.Dal/Abstract/ICompanyInformationDal.cs
@@ -0,0 +1,12 @@
+﻿using IsKaiser.Management.Core.DataAccess;
+using IsKaiser.Management.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IsKaiser.Management.Dal.Abstract
+{
+    public interface ICompanyInformationDal:IEntityRepository<CompanyInformation>
+    {
+    }
+}
